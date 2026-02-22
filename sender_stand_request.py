4991d92bb1916e0f9b1016e30a8e8a753efe5358
Erik Martin Bustos Piñero <erikmbp@gmail.com>
2026-02-22 10:55:54 -0600

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,37 @@
+from http.client import responses
+
+import configuration
+import requests
+import data
+
+
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count":20})
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+
+# response = get_users_table()
+# print(response.status_code)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+#response = post_new_user(data.user_body)
+#print(response.status_code)
+#print(response.json())
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+#response = post_products_kits(data.product_ids)
+#print(response.status_code)
+#print(response.json())
+
